98900178b0fa3bc2f6d11496025d76daf6542dc5
Wang <msj1013654754@gmail.com>
2022-05-24 19:54:22 +0800

    remove gui && reduce apk file size

--- app/src/main/java/github/zerorooot/fuckbilibilivote/Xposed.kt
+++ app/src/main/java/github/zerorooot/fuckbilibilivote/Xposed.kt
@@ -11,15 +11,8 @@ class Xposed : IXposedHookLoadPackage {
             return
         }
         XposedBridge.log("hook ${lpparam.packageName}")
-        val xsp = XSharedPreferences(BuildConfig.APPLICATION_ID, "bili")
-        val isAutoHook = xsp.getBoolean("auto_hook", true)
-        if (isAutoHook) {
-            autoHook(lpparam)
-            return
-        }
-        val className = xsp.getString("bili_class_name", "")
-        val methodName = xsp.getString("bili_class_method", "")
-        biliHook(className, methodName, lpparam)
+        autoHook(lpparam)
+
     }
 
 
@@ -32,30 +25,11 @@ class Xposed : IXposedHookLoadPackage {
         method.isAccessible = true
         XposedBridge.hookMethod(method, object : XC_MethodHook() {
             override fun afterHookedMethod(param: MethodHookParam) {
-                XposedBridge.log("auto hook bili vote success")
+                XposedBridge.log("auto hook bili vote success ${param.result}")
                 param.result = null
             }
         })
     }
 
-    @Throws(NoSuchMethodException::class)
-    private fun biliHook(className: String?, methodName: String?, lpparam: LoadPackageParam) {
-        val hookClass = XposedHelpers.findClass(className, lpparam.classLoader)
-        val viewProgressReplyClass = XposedHelpers.findClass(
-            "com.bapis.bilibili.app.view.v1.ViewProgressReply",
-            lpparam.classLoader
-        )
-        val method = hookClass.getDeclaredMethod(
-            methodName, viewProgressReplyClass,
-            Long::class.javaPrimitiveType,
-            Long::class.javaPrimitiveType
-        )
-        method.isAccessible = true
-        XposedBridge.hookMethod(method, object : XC_MethodHook() {
-            override fun beforeHookedMethod(param: MethodHookParam) {
-                XposedBridge.log("hook bili vote success")
-                param.args[0] = null
-            }
-        })
-    }
+
 }
