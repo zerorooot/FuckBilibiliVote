5db1da7e0d1921b2841c6c986fbd9d1573a8e18d
Wang <msj1013654754@gmail.com>
2022-09-15 17:27:07 +0800

    update

--- app/src/test/java/github/zerorooot/fuckbilibilivote/a.java
+++
@@ -1,9 +0,0 @@
-package github.zerorooot.fuckbilibilivote;
-
-import java.util.ArrayList;
-
-public class a {
-    public static void main(String[] args) {
-        ArrayList<Object> arrayList = new ArrayList<>();
-    }
-}
